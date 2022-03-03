7ec35411837e812af38586fd20f284a4b66621db
xk2102 <xk2102s@gmail.com>
2022-03-03 17:03:04 +0200

    added addPlan

--- src/components/AddPlan.tsx
+++ src/components/AddPlan.tsx
@@ -9,31 +9,29 @@ export default function AddPlan() {
   const COLORS = ["#3689e6", "#28bca3", "#68b723", "#f9c440", "#ffa154", "#ed5353", "#de3e80", "#a56de2", "#8a715e", "#667885"];
   return (
     <div className={styles.addPlan}>
-      {addPlanExpand && (
-        <div className={`animate ${styles.content}`}>
-          <div className={styles.actions}>
-            <button>RESET</button>
-            <button>ADD</button>
-          </div>
-
-          <form>
-            <input placeholder="Plan name..."></input>
-          </form>
-          <div className={styles.colors}>
-            {COLORS.map((color, index) => (
-              <div key={index} className={styles.color} style={{ backgroundColor: color }}></div>
-            ))}
-          </div>
-        </div>
-      )}
-
       <div className={styles.header} onClick={() => setAddPlanExpand(!addPlanExpand)}>
         <div className={styles.image}>
           <BsPlusLg />
         </div>
         <div className={styles.label}>Add Plan</div>
-        <div className={styles.image}>{addPlanExpand ? <MdExpandLess /> : <MdExpandMore />}</div>
+        <div className={styles.image}>{addPlanExpand ? <MdExpandMore /> : <MdExpandLess />}</div>
       </div>
+      {addPlanExpand && (
+        <div className={`animate ${styles.content}`}>
+          <div className={styles.colors}>
+            {COLORS.map((color, index) => (
+              <div key={index} className={styles.color} style={{ backgroundColor: color }}></div>
+            ))}
+          </div>
+          <form>
+            <input placeholder="Plan name..."></input>
+          </form>
+          <div className={styles.actions}>
+            <button>RESET</button>
+            <button>ADD</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
